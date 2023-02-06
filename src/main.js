acf654b34537d4ef8eac2fe62580063021f8182a
WacharaponChaiwan <teung018@gmail.com>
2023-02-07 01:20:57 +0700

    yay

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import"bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
-//import "./assets/main.css";
 
+import"bootstrap/dist/css/bootstrap.css";
+import"bootstrap/dist/js/bootstrap.js";
+
+import"bootstrap-icons/font/bootstrap-icons.css";
 const app = createApp(App);
 
 app.use(createPinia());
